e8211f9f66bcf7df1694346c09b054ce2596d5d7
LIStahura <161784501+LIStahura@users.noreply.github.com>
2025-04-09 08:46:17 +0000

    fixed sqare changing colour contantly. had to remove " square.addEventListener('mouseout', (event) => changecolour('red'));          ////ensures the color changes back to red when mouse is moved off by
    /// had 'square.addEventListener('mouseout', (event) => changecolour('red')); 'ensuring  changes back to red when mouse is moved off - was not working would not keep changing colour until line was removed. //////

--- js/flatlandscript.js
+++ js/flatlandscript.js
@@ -3,17 +3,17 @@ let square = document.getElementById('square');
 let words = document.getElementById('words');
 
 /* Add event listener */
-square.addEventListener('click', (event) => clicked());     ////added event listener to square///
-square.addEventListener('mouseover', (event) => changecolour('grey'));     ////added event listener to square for changing colour when mouse hovered over///
-square.addEventListener('mouseout', (event) => changecolour('red'));     ////added event listener to square changes colour back after mouse is moved off ///
+square.addEventListener('click', (event) => clicked());                         ////added event listener to square///
+square.addEventListener('mouseover', (event) => changecolour('deepred'));      ////added event listener to square for changing colour when mouse hovered over///
+/// had 'square.addEventListener('mouseout', (event) => changecolour('red')); 'ensuring  changes back to red when mouse is moved off - was not working would not keep changing colour until line was removed. //////
 
 document.addEventListener('DOMContentLoaded', () => {
     greeting(); // Call the greeting function when the page loads
 });
 
-function changecolour(colour) {        ////added function to change colour of square when mouse is hovered over
-   square.style.backgroundColor = colour; // Change the background color of the square
- }
+function changecolour(colour) {                      ////added function to change colour of square when mouse is hovered over
+   square.style.backgroundColor = colour;           // Change the background color of the square
+}
 
 function greeting() {
     // console.log("Welcome to Flatland. I am Square.");
@@ -29,16 +29,15 @@ console.log("Script file loaded.");
    1st-version, simple message added to innerHTML*///
 
    /////2nd-version, use buzzword generator */
-   function clicked () {
-      //let msg = "build a<br>"
-      let msg = "build a<br>" + createBuzzwordPhrase();
-       // console.log(msg);
-      words.innerHTML = msg;
-    }
+function clicked () {
+   //let msg = "build a<br>"
+    let msg = "build a<br>" + createBuzzwordPhrase();
+    // console.log(msg);
+    words.innerHTML = msg;
+}
      
    
-/* This function is supplied in the supporting text file.
-   students will only have to copy-n-paste the function */
+/* This function is supplied in the supporting text file.*/
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
